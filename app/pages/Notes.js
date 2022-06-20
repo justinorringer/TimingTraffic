e8e353aa99445706bb64f0e0b1b0c4a55a9654bd
justinorringer <justinorringer@gmail.com>
2022-06-20 01:59:03 -0400

    Fixed screen view misalignment

--- app/pages/Notes.js
+++ app/pages/Notes.js
@@ -30,8 +30,7 @@ export default function Notes() {
                     onChangeText={setNote}
                     style={[styles.textInput]}
                     multiline
-                    autoFocus
-                    selectionColor="#000"
+                    selectionColor="#00f"
                 />
             </View>
         </SafeAreaView>
